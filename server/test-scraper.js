b8d3d01d1025a6c346d160369d5dfec583c1e9e5
Rafli <rafli.rajendra93@gmail.com>
2026-08-31 16:36:02 +0700

    Remove diagnostic test files

--- server/test-scraper.js
+++
@@ -1,7 +0,0 @@
-const { ScraperService } = require('./dist/services/scraper.service');
-
-ScraperService.fetchItemDetails('192013652', '53556643718').then(r => {
-  console.log(JSON.stringify(r, null, 2));
-}).catch(e => {
-  console.error('Error:', e.message);
-});
